53ee6b33cbc2937d15faa827826aa1219ac5c823
Утемуратов Е.К. <UtemuratovYe@csdepo.net>
2025-01-10 14:47:46 +0500

    first commit

---
+++ src/test/java/kz/bitlab/kuber/KuberApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.kuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
